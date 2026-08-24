b23bc5b539a7c71264d7d3bec4f2cc572083278e
Sanjay-D-works <sanjaydevarajworks@gmail.com>
2026-08-24 22:57:55 +0530

    Using serializers to convert data into json format

--- api/views.py
+++ api/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from django.http import JsonResponse
+from students.models import Students 
+from .serializers import StudentsSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET'])
 def studentsView(request):
-    students = {
-        'id' : 1,
-        'name' : 'sanjay',
-        'class' : 'Computer Science'
-    }
-    return JsonResponse(students)
+    if request.method == 'GET':
+         # Get all the data form the studenht table
+        students = Students.objects.all()
+        serializer = StudentsSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
