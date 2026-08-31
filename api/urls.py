989e3a5e8fe04e70405bc47e98934b377cabdaf8
Sanjay-D-works <sanjaydevarajworks@gmail.com>
2026-08-31 17:59:55 +0530

    using an Modelviewset to reduce an code structure

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
 
 
-urlpatterns = [ 
+router = DefaultRouter()
+router.register('employees', views.EmployeeViewset, basename = 'employee')
+
+
+urlpatterns = [
     path('students/', views.studentsView),
     path('students/<int:pk>/', views.studentsDetailsView),
 
-    path('employees/', views.Employees.as_view()),
-    path('employees/<int:pk>/', views.EmployeesDetails.as_view()),
+    # path('employees/', views.Employees.as_view()),
+    # path('employees/<int:pk>/', views.EmployeesDetails.as_view()),
+
+
+    path('', include(router.urls))
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-# from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from django.http import JsonResponse
 from students.models import Students 
 from .serializers import StudentsSerializer, EmployeeSerializer
@@ -8,7 +8,7 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from employees.models import Employee
 from django.http import Http404
-from rest_framework import mixins, generics
+from rest_framework import mixins, generics, viewsets
 
 ### Function based views
 
@@ -120,6 +120,7 @@ class EmployeesDetails(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixin
         return self.destroy(request, pk)
 """ 
 
+"""
 ### Generics
 
 class Employees(generics.ListCreateAPIView):
@@ -127,12 +128,51 @@ class Employees(generics.ListCreateAPIView):
     serializer_class = EmployeeSerializer
 
 
-
 class EmployeesDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     lookup_field = 'pk'
-   
+
+"""
 
 
-         
+### ViewSet
+
+# class EmployeeViewset(viewsets.ViewSet):
+#     def list(self, request):
+#         queryset = Employee.objects.all()
+#         serializer = EmployeeSerializer(queryset, many=True)
+#         return Response(serializer.data)
+
+#     def create(self, request):
+#         serializer = EmployeeSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors)
+
+#     def retrieve(self, request, pk=None):
+#         employee = get_object_or_404(Employee, pk=pk)
+#         serailizer = EmployeeSerializer()
+#         return Response(serailizer.data, status=status.HTTP_200_OK)
+
+#     def update(self, request, pk=None):
+#         employee = get_object_or_404(Employee, pk=pk)
+#         serializer = EmployeeSerializer(employee, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
+
+#     def delete(self, request, pk=None):
+#         employee = get_object_or_404(Employee, pk=pk)
+#         employee.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    
+
+### Moselset
+
+class EmployeeViewset(viewsets.ModelViewSet):
+    
+
